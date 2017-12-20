bb12ba2454dab67617e5301605642664b7f7b969
jim-at-jibba <jim@justjibba.net>
2017-12-20 15:49:48 +0000

    Add log to unicorn button

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -4,7 +4,9 @@ import { Footer, FooterTab, Button, Thumbnail } from "native-base";
 export default () => (
   <Footer style={{ height: 70, borderTopWidth: 0, backgroundColor: "white" }}>
     <FooterTab>
-      <Button full>
+      <Button full
+        onPress={() => console.log('Time for a unicorn party!')}
+      >
         <Thumbnail square source={require("../../images/unicorn.png")} />
       </Button>
     </FooterTab>
