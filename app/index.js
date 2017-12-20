440ee069a9d995c3325e32bd3ebd2892cea009d4
jim-at-jibba <jim@justjibba.net>
2017-12-20 15:25:01 +0000

    Added RN extended stylesheet and colours

--- app/index.js
+++ app/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import EStylesheet from "react-native-extended-stylesheet";
 import Kitchen from "./screens/kitchen/kitchen";
+
+EStylesheet.build({
+  $pink: "#c49ae9",
+  $beige: "#ffd0a1",
+  $blue: "#7ee1e6",
+  $darkGrey: "#51565f",
+  $grey: "#aab1ba",
+  $cream: "#f2edda"
+});
 
 export default App => <Kitchen />;
