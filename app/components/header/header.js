73877a792e67e8adce4892aac0320fe8121a387b
jim-at-jibba <jim@justjibba.net>
2017-12-22 18:57:08 +0000

    Change header colors

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import { Header, Title, Body } from "native-base";
 import styles from "./styles";
 
-export default () => (
-  <Header style={{ backgroundColor: "#c49ae9" }}>
+export default () =>
+  <Header style={{ backgroundColor: "#7ee1e6" }}>
     <Body>
       <Title style={{ color: "#51565f" }}>Rowans Kitchen</Title>
     </Body>
-  </Header>
-);
+  </Header>;
