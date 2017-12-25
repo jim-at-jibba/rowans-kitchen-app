e6c061ca4af4453721e079a7595cd532b9cd3ccb
jim-at-jibba <jim@justjibba.net>
2017-12-25 20:24:11 +0000

    Added light and oven images. Simple logic to change image based on state

--- app/screens/kitchen/kitchen.js
+++ app/screens/kitchen/kitchen.js
@@ -1,14 +1,74 @@
 import React, { Component } from "react";
-import { Container, Content, Body, Text } from "native-base";
+import { Container, Content, Body, Text, Thumbnail } from "native-base";
+import { View, TouchableOpacity, Dimensions } from "react-native";
+const { width, height } = Dimensions.get("window");
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 
+const gridWidth = width / 2 - 15;
+
 export default class Kitchen extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      light: false
+    };
+  }
   render() {
+    const bulbIcon = this.state.light
+      ? require("../../images/light-bulb.png")
+      : require("../../images/light-bulb-off.png");
     return (
       <Container>
         <Header />
-        <Content />
+        <Content padder>
+          <View
+            style={{
+              justifyContent: "space-between",
+              flexDirection: "row",
+              flexWrap: "wrap"
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                width: gridWidth,
+                height: 120,
+                marginBottom: 9,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#c49ae9"
+              }}
+              onPress={() => console.log("Setting the ligths")}
+            >
+              <Thumbnail square source={bulbIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                width: gridWidth,
+                height: 120,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#c49ae9"
+              }}
+              onPress={() => console.log("Setting the ligths")}
+            >
+              <Thumbnail square source={require("../../images/oven.png")} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                width: gridWidth,
+                height: 120,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#c49ae9"
+              }}
+              onPress={() => console.log("Setting the ligths")}
+            >
+              <Thumbnail square source={require("../../images/oven.png")} />
+            </TouchableOpacity>
+          </View>
+        </Content>
         <Footer />
       </Container>
     );
